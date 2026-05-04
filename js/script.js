f6267b693e17b0e7a41bdc061457f252e2df7cc6
Sascha S. <sschmitt1977@googlemail.com>
2026-05-04 16:13:43 +0200

    added favicon, dialog shows selected photo and number

--- js/script.js
+++ js/script.js
@@ -1,11 +1,16 @@
 const dialogRef = document.getElementById("chosen_photo_dialog")
+let total = document.getElementById("photo_index").children.length;
 
-function photo_view_dialog() {
+function photo_view_dialog(clicked) {
+    let img = document.getElementById("chosen_photo" + clicked);
+
     dialogRef.showModal();
-    dialogRef.classList.add('full_view_dialog.opened');
+    dialogRef.classList.add('full_view_dialog_opened');
+    document.getElementById("chosen_photo").src = img.src;
+    document.getElementById("photo_number").innerHTML = clicked + "/" + total;
 }
 
 function close_dialog() {
-    dialogRef.classList.remove('full_view_dialog.opened');
+    dialogRef.classList.remove('full_view_dialog_opened');
     dialogRef.close();
 }
